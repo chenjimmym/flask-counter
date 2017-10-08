83d41ec50e597855668ae4478281c53bd5f3558f
Jimmy <chen.jimmym@gmail.com>
2017-10-08 10:05:39 -0700

    done

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, session, redirect
+
+app = Flask(__name__)
+app.secret_key = 'aSecret'
+
+@app.route('/')
+def indexPage():
+    session['count'] += 1
+    return render_template('index.html')
+@app.route('/reset', methods=['POST'])
+def resetCount():
+    session['count'] = 0
+    return redirect('/')
+app.run(debug=True)
